170c0b64955c40de7f6a16c8228097e059c38623
Havvksta <burlacuradu1990@gmail.com>
2026-01-19 20:49:39 +0200

    Update 4

--- js/data.js
+++ js/data.js
@@ -1,6 +1,6 @@
 const DATA = {
-  "generatedAt": "2026-01-15T14:39:25.307412",
-  "count": 1,
+  "generatedAt": "2026-01-19T18:45:42.671278",
+  "count": 2,
   "products": [
     {
       "asin": "B0DQSD7YQC",
@@ -11,7 +11,18 @@ const DATA = {
       "currency": "USD",
       "link": "https://www.amazon.com/dp/B0DQSD7YQC",
       "image": "https://m.media-amazon.com/images/I/81JPZdpeH%2BL._SL1500_.jpg",
-      "updated": "2026-01-15"
+      "updated": "2026-01-19"
+    },
+    {
+      "asin": "B0DS6V1YSY",
+      "name": "RTX 5070",
+      "oldPrice": 593,
+      "newPrice": 549,
+      "dropPct": 7,
+      "currency": "USD",
+      "link": "https://www.amazon.com/dp/B0DS6V1YSY",
+      "image": "https://m.media-amazon.com/images/I/81Si31LMp5L._SL1500_.jpg",
+      "updated": "2026-01-19"
     }
   ]
 };
