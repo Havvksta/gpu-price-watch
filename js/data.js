a7bf96645c8504292ded6f0b24a86ebd78449c3e
RaduBurlacu <radu.burlacu@finastra.com>
2026-01-27 22:17:55 +0200

    update

--- js/data.js
+++ js/data.js
@@ -1,28 +1,28 @@
 const DATA = {
-  "generatedAt": "2026-01-19T18:45:42.671278",
+  "generatedAt": "2026-01-27T20:13:27.593493",
   "count": 2,
   "products": [
     {
       "asin": "B0DQSD7YQC",
-      "name": "RTX 5080",
+      "name": "ASUS ROG Astral GeForce RTX\u2122 5080 OC Edition Gaming Graphics Card (PCIe\u00ae 5.0, 16GB GDDR7, HDMI\u00ae/DP 2.1, 3.8-Slot, 4-Fan Design, Axial-tech Fans, Patented Vapor Chamber, Phase-Change GPU Thermal pad)",
       "oldPrice": 1999,
-      "newPrice": 1799,
-      "dropPct": 10,
+      "newPrice": 1899,
+      "dropPct": 5,
       "currency": "USD",
       "link": "https://www.amazon.com/dp/B0DQSD7YQC",
       "image": "https://m.media-amazon.com/images/I/81JPZdpeH%2BL._SL1500_.jpg",
-      "updated": "2026-01-19"
+      "updated": "2026-01-27"
     },
     {
-      "asin": "B0DS6V1YSY",
-      "name": "RTX 5070",
-      "oldPrice": 593,
-      "newPrice": 549,
-      "dropPct": 7,
+      "asin": "B0F5BBGCSZ",
+      "name": "GIGABYTE GeForce RTX 5060 Ti WINDFORCE OC 16G Graphics Card, Cooling System, 16GB 128-bit GDDR7, PCIe 5.0, by NVIDIA, DisplayPort & HDMI - Video Output Interface, GV-N506TWF2OC-16GD Video Card",
+      "oldPrice": 449,
+      "newPrice": 349,
+      "dropPct": 22,
       "currency": "USD",
-      "link": "https://www.amazon.com/dp/B0DS6V1YSY",
-      "image": "https://m.media-amazon.com/images/I/81Si31LMp5L._SL1500_.jpg",
-      "updated": "2026-01-19"
+      "link": "https://www.amazon.com/dp/B0F5BBGCSZ",
+      "image": "https://m.media-amazon.com/images/I/71Oem3RNSZL._SL1500_.jpg",
+      "updated": "2026-01-27"
     }
   ]
 };
